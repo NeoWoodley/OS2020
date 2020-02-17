cbbfc6a41c1a96e21f44b05655f38cf39d8cf809
tracer-nju <tracer@nju.edu.cn>
2020-02-17 20:17:49 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 4fd2397e342d400ca2188d8d937e99c1   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-28-generic       Architecture: x86-64  20:17:49 up 41 min,  3 users,  load average: 0.66, 0.68, 0.55

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -2,6 +2,9 @@
 #include <assert.h>
 
 int main(int argc, char *argv[]) {
+  pid_t curr_pid = getpid(void);
+  printf("PID:%d\n",curr_pid);
+
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
     printf("argv[%d] = %s\n", i, argv[i]);

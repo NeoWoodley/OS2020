bc92f24ec580b4083455a4911def205b5a99af7f
tracer-nju <tracer@nju.edu.cn>
2020-02-17 21:12:32 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 4fd2397e342d400ca2188d8d937e99c1   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-28-generic       Architecture: x86-64  21:12:32 up  1:36,  3 users,  load average: 1.39, 1.53, 1.54

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -3,9 +3,15 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#define --version -V
+#define --show-pids -p
+#define --numeric-sort -n
+
 int main(int argc, char *argv[]) {
   pid_t curr_pid = getpid();
   printf("PID:%d\n",curr_pid);
+
+  
 
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);

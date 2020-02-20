99c99344941e97ef72782942a0515ace03a3825f
tracer-nju <tracer@nju.edu.cn>
2020-02-20 09:07:31 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: f011ea4733984cf1bb4fbdda0b5ce9f1   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-40-generic       Architecture: x86-64  09:07:31 up 59 min,  4 users,  load average: 0.33, 0.18, 0.22

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -9,6 +9,7 @@
 struct node {
     int pid;
 	char* name;
+	int ppid;
 };
 
 /*
@@ -77,10 +78,10 @@ int main(int argc, char *argv[]) {
 
   while((entry=readdir(dir))) {
         if(find_num(entry->d_name) == 0) {
-            printf("File %s :Not a process file!\n", entry->d_name);
+            // printf("File %s :Not a process file!\n", entry->d_name);
         }
         else {
-            printf("File %s :Process!\n", entry->d_name);
+            // printf("File %s :Process!\n", entry->d_name);
 			infolib[count].pid = str2int(entry->d_name);
             infolib[count].name = entry->d_name;
             count ++;

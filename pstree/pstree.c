d8ed96cef1c9a715917ff25fbe8e0d01baa20c22
tracer-nju <tracer@nju.edu.cn>
2020-02-20 22:01:08 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: d4d0b66afc1446d2881c0e3db07857db   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-40-generic       Architecture: x86-64  22:01:08 up 51 min,  3 users,  load average: 0.18, 0.33, 0.41

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -102,8 +102,7 @@ int main(int argc, char *argv[]) {
       if(file) {
 	      getline(&line, &line_buf_size, file);
 		  printf("name? %s\n",line);
-		  sprintf((char*)(infolib[i].name), "%s",&line[6]);
-		  //infolib[i].name = &line[6];
+		  infolib[i].name = &line[6];
 		  
 		  for(int j = 0; j < 5; j ++) {
 		      getline(&line, &line_buf_size, file);

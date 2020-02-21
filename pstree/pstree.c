0c721dd7ebca73bcc899b7e680a765eb22dc8dff
tracer-nju <tracer@nju.edu.cn>
2020-02-21 08:47:39 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: dade19e3b5584cd4b3a2715b27b2bdcf   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-40-generic       Architecture: x86-64  08:47:39 up 42 min,  3 users,  load average: 0.30, 0.55, 0.43

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -118,7 +118,7 @@ int main(int argc, char *argv[]) {
 	  infolib[i].ppid=str2int(&ppid[6]);
 	  } 	  
 
-      printf("File %s PID %d PPID %d\n", infolib[i].name, infolib[i].pid, infolib[i].ppid);
+//      printf("File %s PID %d PPID %d\n", infolib[i].name, infolib[i].pid, infolib[i].ppid);
   }
   free(path);
   free(name);

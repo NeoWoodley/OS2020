63a4d75e472d8c7a1ac470b36b71b65e3cf7ade9
tracer-nju <tracer@nju.edu.cn>
2020-02-22 15:13:23 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 3a346173aecc4eb3b8be229def4c04f4   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-40-generic       Architecture: x86-64  15:13:23 up  4:50,  3 users,  load average: 0.08, 0.16, 0.17

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -14,7 +14,7 @@ struct node {
 	int ppid;
 	struct node* child;
 	struct node* peer;
-//	int depth;
+	int depth;
 };
 
 struct node* findpeertail(struct node* Node) {
@@ -100,6 +100,10 @@ int main(int argc, char *argv[]) {
         }
   }
 
+  for(int i = 0; i < count; i ++){
+      infolib[i].depth = 0;
+  }
+
   printf("Total process number:%d\n",count);
 
   closedir(dir);
@@ -133,12 +137,12 @@ int main(int argc, char *argv[]) {
 	  for(int j = i+1; j < count; j ++) {
 	      if(infolib[i].child == NULL && infolib[j].ppid == infolib[i].pid) {
 		      infolib[i].child = (struct node*)&infolib[j];
-	//		  infolib[j].depth = infolib[i].depth + 1; 
+			  infolib[j].depth = infolib[i].depth + 1; 
 		  }
 		  else if(infolib[i].child != NULL && infolib[j].ppid == infolib[i].pid) {
               temp = findpeertail((struct node*)&(infolib[i].child->peer));
 			  temp->peer = (struct node*)&infolib[j];
-	//		  infolib[j].depth = infolib[i].depth; 
+			  infolib[j].depth = infolib[i].depth; 
 		  }
 	  }
       

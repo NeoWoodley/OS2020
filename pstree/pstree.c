140243058bf3060143fe3f3e5b713ecc886fe897
tracer-nju <tracer@nju.edu.cn>
2020-02-22 15:16:42 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 3a346173aecc4eb3b8be229def4c04f4   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-40-generic       Architecture: x86-64  15:16:42 up  4:53,  3 users,  load average: 0.23, 0.16, 0.17

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -59,7 +59,7 @@ int find_num(char *str) {
 void buildtree(struct node* Node) {
 	struct node* temp;
     if(Node->child == NULL) {
-	    printf("--%s{%d}\n",&Node->name[0],Node->pid);
+	    printf("--%s{%d}(%d)\n",&Node->name[0],Node->pid,Node->depth);
 	}
 	
 	else if(Node->child != NULL) {

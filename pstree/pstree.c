29b99f6793b91021cda6ebc342d36351be66688b
tracer-nju <tracer@nju.edu.cn>
2020-02-22 22:08:29 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 3a346173aecc4eb3b8be229def4c04f4   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-40-generic       Architecture: x86-64  22:08:29 up 11:45,  4 users,  load average: 0.11, 0.18, 0.14

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -173,10 +173,10 @@ int main(int argc, char *argv[]) {
   assert(infolib[0].child != NULL);
 
   for(int i = 0; i < count; i ++) {
-//	  if(infolib[i].depth == 0) {
+	  if(infolib[i].depth == 0) {
           buildtree((struct node*)&infolib[i]);
 		  printf("-------------------------------------------------------------\n");
-//	  }
+	  }
 //	  printf("%s{%d}--ppid{%d}--depth{%d}\n",&infolib[i].name[0], infolib[i].pid, infolib[i].ppid, infolib[i].depth);
   }
 

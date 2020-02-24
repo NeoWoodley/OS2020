a1b058cf46b515153f358851289b5fa7aed02c84
tracer-nju <tracer@nju.edu.cn>
2020-02-24 21:18:06 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: ecf1b08138b946ebb2ded978906d0e8d   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-40-generic       Architecture: x86-64  21:18:07 up  3:33,  3 users,  load average: 0.42, 0.33, 0.34

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -192,28 +192,28 @@ int main(int argc, char *argv[]) {
   for(int i = 0; i < count; i ++) {
 	  for(int j = 0; j < count; j ++) {
 	      if(infolib[i].child == NULL && infolib[j].ppid == infolib[i].pid) {
-			  printf("-----S1\n");
+//			  printf("-----S1\n");
 		      infolib[i].child = (struct node*)&infolib[j];
 			  infolib[j].depth = infolib[i].depth + 1; 
 		  }
 		  else if(infolib[i].child != NULL && infolib[j].ppid == infolib[i].pid) {
-			  printf("-----S2\n");
+//			  printf("-----S2\n");
 			  if(infolib[i].child->peer == NULL) {
-			      printf("-----S2-1\n");
+//			      printf("-----S2-1\n");
 			      infolib[i].child->peer = (struct node*)&infolib[j];
 			  }
 			  else if(infolib[i].child->peer != NULL){
-			      printf("-----S2-2\n");
+//			      printf("-----S2-2\n");
                   temp = findpeertail((struct node*)&(infolib[i].child->peer));
 			      temp->peer = (struct node*)&infolib[j];
-				  printf("@@@@@%s\n",temp->peer->name);
+//				  printf("@@@@@%s\n",temp->peer->name);
 			  }
 			  infolib[j].depth = infolib[i].depth + 1; 
 		  }
 		  else {
-		      printf("-----S3\n");
+//		      printf("-----S3\n");
 		  }
-		  printf("i=%d,j=%d,lib[%d].depth=%d, lib[%d].child:%p, lib[%d].depth=%d\n",i,j,i,infolib[i].depth,i,infolib[i].child,j,infolib[j].depth); 
+//		  printf("i=%d,j=%d,lib[%d].depth=%d, lib[%d].child:%p, lib[%d].depth=%d\n",i,j,i,infolib[i].depth,i,infolib[i].child,j,infolib[j].depth); 
 	  }
   }
 

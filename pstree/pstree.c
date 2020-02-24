43a2cb4c10d7d6e740e04a495b161d267e8f463a
tracer-nju <tracer@nju.edu.cn>
2020-02-24 21:52:58 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: ecf1b08138b946ebb2ded978906d0e8d   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-40-generic       Architecture: x86-64  21:52:58 up  4:08,  3 users,  load average: 0.41, 0.26, 0.25

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -232,6 +232,12 @@ int main(int argc, char *argv[]) {
 	  }
 	  printf("-------------------------------\n");
   }
+  assert(infolib[2].child == NULL);
+  assert(infolib[0].child == NULL);
+  assert(infolib[1].peer == NULL);
+  assert(infolib[2].peer == NULL);
+  assert(infolib[4].peer == NULL);
+  assert(infolib[3].child == NULL);
 
  // assert(infolib[0].child->peer != NULL);
 
@@ -383,12 +389,6 @@ int main(int argc, char *argv[]) {
   }
 */
 
-  assert(infolib[2].child == NULL);
-  assert(infolib[0].child == NULL);
-  assert(infolib[1].peer == NULL);
-  assert(infolib[2].peer == NULL);
-  assert(infolib[4].peer == NULL);
-  assert(infolib[3].child == NULL);
 
   return 0;
   

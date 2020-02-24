70d4d52d3ed1d564a1b8704d581103e7862e923e
tracer-nju <tracer@nju.edu.cn>
2020-02-24 22:04:46 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: ecf1b08138b946ebb2ded978906d0e8d   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-40-generic       Architecture: x86-64  22:04:46 up  4:20,  3 users,  load average: 0.36, 0.31, 0.27

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -88,7 +88,7 @@ void buildtree(struct node* Node, int mode) {
        // printf("%s{%d}\n",&(Node->name[0]), Node->pid);
 		temp =(struct node*) ((struct node*)(Node->child))->peer;
 	    while(temp != NULL) {
-		   printf("??????????????????\n")
+		   printf("??????????????????\n");
 		   buildtree(temp , mode);
 		   temp = (struct node*)(temp->peer);
 		}

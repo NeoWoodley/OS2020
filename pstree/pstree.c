37662d1c181875beaf1e568e705b63d16cf7812c
tracer-nju <tracer@nju.edu.cn>
2020-02-24 20:07:37 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: ecf1b08138b946ebb2ded978906d0e8d   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-40-generic       Architecture: x86-64  20:07:37 up  2:22,  3 users,  load average: 0.64, 0.67, 0.57

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -128,7 +128,7 @@ int main(int argc, char *argv[]) {
 	  infolib[i].child = NULL;
   }
 
-  printf("Total process number:%d\n",count);
+  //printf("Total process number:%d\n",count);
 
   closedir(dir);
 

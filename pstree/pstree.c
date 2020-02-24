8516fc19135411b6606fcb883e1c7be7311d492a
tracer-nju <tracer@nju.edu.cn>
2020-02-24 21:13:45 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: ecf1b08138b946ebb2ded978906d0e8d   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-40-generic       Architecture: x86-64  21:13:45 up  3:29,  3 users,  load average: 0.19, 0.33, 0.35

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -132,7 +132,7 @@ int main(int argc, char *argv[]) {
 	  }
   }
 
-  printf("Total process number:%d\n",count);
+  // printf("Total process number:%d\n",count);
 
   closedir(dir);
 
@@ -181,9 +181,11 @@ int main(int argc, char *argv[]) {
   free(name);
   free(ppid);
 
+  /*
   for(int i = 0; i < count; i ++) {
        printf("name:%s, pid:%d, ppid:%d\n", infolib[i].name, infolib[i].pid, infolib[i].ppid);
   }
+  */
 
   struct node* temp = (struct node*)malloc(sizeof(struct node));
   //struct node* temp;

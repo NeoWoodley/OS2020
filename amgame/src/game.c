0cdd6ad46469af6cbcffdab6bfe59fbd74128767
tracer-nju <tracer@nju.edu.cn>
2020-03-12 11:36:14 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: b9a2ef9e5448427788d3159c997d5bc5   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-40-generic       Architecture: x86-64  11:36:14 up  2:29,  3 users,  load average: 0.69, 0.60, 0.46

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -33,7 +33,7 @@ int main(const char *args) {
 	  } 
 	  
 	  else if (receive == 1 || ((receive | 0x8000) != 0x8001)) 
-          _halt(0);
+          _halt(1);
   }
     //_halt(0);
 		//if(receive != _KEY_NONE && ((receive | 0x8000)!= receive) ) 

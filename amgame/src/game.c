513cf1a688543318ff5854810882c70de78e512d
tracer-nju <tracer@nju.edu.cn>
2020-03-16 09:54:43 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 8860c02823b34c06949aaf1a9003f78b   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-40-generic       Architecture: x86-64  09:54:43 up  2:07,  1 user,  load average: 0.09, 0.44, 0.37

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -19,7 +19,6 @@ int main(const char *args) {
   int h = screen_height();
   int x = 2;
   int y = 2;
-  assert(x != 2);
   //printf("w:%d,h:%d\n",w,h);
   //puts("Press any key to see its key code...\n");
   while (1) {

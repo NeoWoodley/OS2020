32e48fbac1a04ec3b875ea9e1c2440f98c09b92e
tracer-nju <tracer@nju.edu.cn>
2020-03-16 21:59:34 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 9abb54bb92a645578030d56a2292f23e   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-40-generic       Architecture: x86-64  21:59:34 up  3:08,  3 users,  load average: 0.18, 0.24, 0.27

--- libco/co.c
+++ libco/co.c
@@ -1,15 +1,73 @@
 #include "co.h"
+#include "x86.h"
 #include <stdlib.h>
 
-struct co {
+#define STACK_SIZE (1<<10)
+
+/*
+#if __x86_64__
+
+#else
+*/
+
+enum co_status {
+
+    CO_NEW = 1,
+	CO_RUNNING,
+	CO_WAITING,
+	CO_DEAD,
+
 };
 
+struct co {
+
+	char *name;
+	void (*func)(void *);
+	void *arg;
+
+	enum co_status status;
+	struct co *    waiter;
+	jmp_buf        context;
+	uint8_t        stack[STACK_SIZE];
+
+};
+
+struct co *current;
+
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
-  return NULL;
+
+	assert(name != NULL && func != NULL && arg != NULL);
+	struct co *new_co = (struct co*)malloc(sizeof(struct co));
+	new_co->name = name;
+	new_co->func = func;
+	new_co->arg = arg;
+	new_co->status = CO_NEW;
+    
+    return new_co;
+
 }
 
 void co_wait(struct co *co) {
+
+	current->status = CO_WAITING;
+	struct co *old_current = current;
+	co->status = CO_RUNNING;
+	current = co;
+	current->func(current->arg);
+	current->status = CO_DEAD;
+	current = old_current;
+	free(co);
+
 }
 
 void co_yield() {
+
+	current->status = CO_WAITING;
+    int val = setjmo(current->context);
+    if (val == 0) {
+        	
+	}
+    else {
+	
+	}	
 }

386298810cc83903260569e8604636e9c98689f2
tracer-nju <tracer@nju.edu.cn>
2020-03-20 10:33:31 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: a69b3eae14874e8cb06d00200408342f   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  10:33:31 up  2:24,  3 users,  load average: 0.04, 0.12, 0.19

--- libco/co.c
+++ libco/co.c
@@ -123,9 +123,6 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg) {
 	assert(name != NULL && func != NULL && arg != NULL);
 	struct co *new_co = (struct co*)malloc(sizeof(struct co));
 	assert(new_co->name != NULL);
-    strncpy(new_co->name, name, i);
-	char end = '\0';
-	strcat(new_co->name, &end);
     printf("HHHHHHHHHHHHHHHHHH__%d __HHHHHHHHHHHHHHHHHHHHHH\n", i);
 	new_co->func = func;
 	new_co->arg = arg;

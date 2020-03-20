4aea591c943494c2e64fa5a2115b878961a398f8
tracer-nju <tracer@nju.edu.cn>
2020-03-20 13:58:55 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: a69b3eae14874e8cb06d00200408342f   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  13:58:55 up  5:50,  3 users,  load average: 0.47, 0.38, 0.20

--- libco/co.c
+++ libco/co.c
@@ -103,8 +103,8 @@ void rand_choose(struct co* head, struct co* candidate) {
     }
     
 	srand((unsigned)time(NULL));
-	int index = rand() % count;
 		printf("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH\n");
+	int index = rand() % count;
 	struct co* pool = rand_pool;
 	for(int i=0; i < index; i ++) {
 	    pool = pool->brother;

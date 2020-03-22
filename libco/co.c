142091b1d1b522601da2ba135965d1870c9d0bd5
tracer-nju <tracer@nju.edu.cn>
2020-03-22 22:07:21 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 70eade2a1d494f129453a49d2815ad83   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  22:07:21 up 12:43,  3 users,  load average: 0.81, 0.92, 0.61

--- libco/co.c
+++ libco/co.c
@@ -246,7 +246,7 @@ void co_yield() {
 		        printf("Another co was chosen and it is a new co!\n");
 #endif
 				assert(new_co.brother->stack != NULL && new_co.brother->func != NULL && new_co.brother->arg != NULL);
-#ifdef DEBUd
+#ifdef DEBUG
 		        printf("stack_space 1:%p\n", &new_co.brother->stack[0]);
 		        printf("stack_space 2:%p\n", &new_co.brother->stack[STACK_SIZE-1]);
 		        printf("func_entry:%p\n", new_co.brother->func);

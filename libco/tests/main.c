976b23e640bc1e2ab76fef7c6ec78654aafbed69
tracer-nju <tracer@nju.edu.cn>
2020-03-22 21:32:18 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 70eade2a1d494f129453a49d2815ad83   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  21:32:18 up 12:08,  3 users,  load average: 0.27, 0.46, 0.54

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -17,6 +17,7 @@ static int get_count() {
 static void work_loop(void *arg) {
     const char *s = (const char*)arg;
     for (int i = 0; i < 100; ++i) {
+		/*
 		putchar('@');
 		putchar('@');
 		putchar('@');
@@ -25,7 +26,8 @@ static void work_loop(void *arg) {
 		putchar('@');
 		putchar('@');
 		putchar('\n');
-        printf("%s%d  ", s, get_count());
+		*/
+        printf("%s%d ", s, get_count());
         add_count();
         co_yield();
     }

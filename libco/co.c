cca1aa2b58191f3f4c4252a48a2cfbcfee206764
tracer-nju <tracer@nju.edu.cn>
2020-03-22 19:10:53 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 70eade2a1d494f129453a49d2815ad83   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  19:10:53 up  9:46,  3 users,  load average: 0.42, 0.32, 0.23

--- libco/co.c
+++ libco/co.c
@@ -243,7 +243,9 @@ void co_yield() {
 #endif
 			} while(strcmp(new_co.next->name, current->name) == 0);
 			assert(new_co.brother->status == CO_NEW || new_co.brother->status == CO_WAITING);
-			
+#ifdef DEBUG
+		        printf("Another co was chosen\n");
+#endif
 			if (new_co.brother->status == CO_NEW) {
 			    stack_switch_call(&new_co.brother->status, new_co.brother->func, (uintptr_t)new_co.brother->arg);
 			}

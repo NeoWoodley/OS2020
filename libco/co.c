53564a197fcf91e575f20f45a4ab5033c6d47160
tracer-nju <tracer@nju.edu.cn>
2020-03-22 21:29:08 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 70eade2a1d494f129453a49d2815ad83   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  21:29:08 up 12:05,  3 users,  load average: 0.34, 0.64, 0.61

--- libco/co.c
+++ libco/co.c
@@ -246,13 +246,13 @@ void co_yield() {
 		        printf("Another co was chosen and it is a new co!\n");
 #endif
 				assert(new_co.brother->stack != NULL && new_co.brother->func != NULL && new_co.brother->arg != NULL);
-#ifdef DEBUG
+#ifdef DEBUd
 		        printf("stack_space 1:%p\n", &new_co.brother->stack[0]);
 		        printf("stack_space 2:%p\n", &new_co.brother->stack[STACK_SIZE-1]);
 		        printf("func_entry:%p\n", new_co.brother->func);
 		        printf("arg:%s\n", (char*)new_co.brother->arg);
 #endif
-			    stack_switch_call(&new_co.brother->stack[STACK_SIZE-1], new_co.brother->func, (uintptr_t)new_co.brother->arg);
+			    stack_switch_call(&new_co.brother->stack[STACK_SIZE-1], new_co.brother->func, &new_co.brother->arg);
 				assert(0);
 			}
 

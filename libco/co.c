bac5d9af81ff8bfb1492c4b2c74d5b6ddfbb06dd
tracer-nju <tracer@nju.edu.cn>
2020-03-23 20:05:53 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 7f6fdf97a28849e2839a5bdd11423fe6   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  20:05:53 up  1:01,  6 users,  load average: 1.19, 1.08, 0.69

--- libco/co.c
+++ libco/co.c
@@ -257,7 +257,7 @@ void co_yield() {
 
         int val = setjmp(current->context);
         if (val == 0) {
-#ifdef DEBUG
+#ifdef TEST
 		printf("The return value of setjmp is 0 | The current co is %s\n", current->name);
 #endif
             struct co new_co;

cf9e5e57dcd55cda4aa853fc944d6b0f1a9613c8
tracer-nju <tracer@nju.edu.cn>
2020-03-23 15:29:31 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 74633657fb4d487da6f3becde86acf64   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  15:29:31 up  1:29,  3 users,  load average: 0.13, 0.20, 0.22

--- libco/co.c
+++ libco/co.c
@@ -40,7 +40,7 @@ struct co {
 
 	char *name;
 	__attribute__ ((aligned (16))) void (*func)(void *);
-	void *arg;
+	__attribute__ ((aligned (16))) void *arg;
 
 	enum co_status status;
 	struct co *    waiter;

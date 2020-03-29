4d226a1cf98f5d18eb38285dbed27ee1fa49a492
tracer-nju <tracer@nju.edu.cn>
2020-03-29 11:16:03 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: d7753e4997ab4049b244f260e661f61a   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  11:16:03 up  2:00,  3 users,  load average: 0.31, 0.47, 0.31

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -89,6 +89,7 @@ static void consumer(void *arg) {
         if (!q_is_empty(queue)) {
             do_consume(queue);
         }
+		printf("        consumer yield!\n");
         co_yield();
     }
 }

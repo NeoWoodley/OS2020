16c7e0cf8bd7f3564a4d31e6246915d56a8ecc3c
tracer-nju <tracer@nju.edu.cn>
2020-03-30 15:31:31 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 73a74860ec544a25acbc49c3afec7f5d   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  15:31:31 up  1:58,  3 users,  load average: 0.06, 0.18, 0.19

--- libco/co.c
+++ libco/co.c
@@ -394,7 +394,7 @@ void co_wait(struct co *co) {
 		return;
 	}
 
-	else if (current != NULL && co->status == CO_DEAD){
+	else if (co->status == CO_DEAD){
 #ifdef BUG
 		printf("###[FREE]:co %s was freed\n",co->name);
 #endif

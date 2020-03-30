3771c329339cb1ebf52d726b991e51030aad8677
tracer-nju <tracer@nju.edu.cn>
2020-03-30 15:08:23 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 73a74860ec544a25acbc49c3afec7f5d   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  15:08:23 up  1:35,  3 users,  load average: 0.20, 0.22, 0.17

--- libco/co.c
+++ libco/co.c
@@ -325,11 +325,11 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg) {
 void co_wait(struct co *co) {
 #ifdef BUG
 	assert(co->name != NULL);
-	//printf("###[WAIT]:co %s was waited\n",co->name);
+	printf("###[WAIT]:co %s was waited\n",co->name);
 #endif
-//#ifdef DEBUG
+#ifdef DEBUG
 	printf("co %s is to be waited, its state is %d\n", co->name, co->status);
-//#endif
+#endif
 	if(current == NULL && co->status != CO_DEAD) {
 		if(co->status == CO_NEW) {
 			co->status = CO_RUNNING;

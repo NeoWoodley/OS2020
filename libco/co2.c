eb6fa82480c6a687de6c2c688a3bb5cb53b36365
tracer-nju <tracer@nju.edu.cn>
2020-03-30 13:36:45 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 73a74860ec544a25acbc49c3afec7f5d   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-42-generic       Architecture: x86-64  13:36:44 up 3 min,  3 users,  load average: 0.88, 0.62, 0.28

---
+++ libco/co2.c
@@ -0,0 +1,11 @@
+#include "co.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
+#include <setjmp.h>
+#include <string.h>
+#include <time.h>
+
+#define KB 1<<10
+#define STACK_SIZE 64*KB

d05ef362932eeb58b6ee19c98b89ce462217359e
tracer-nju <tracer@nju.edu.cn>
2020-04-05 14:57:38 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: d018140fe02d448f86d3895be82cd1c1   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-45-generic       Architecture: x86-64  14:57:38 up  6:16,  1 user,  load average: 0.20, 0.29, 0.29

--- libco/co.c
+++ libco/co.c
@@ -53,9 +53,9 @@ struct co* pool[128];
 struct co current_co;
 struct co* current = &current_co;
 char main_name[5] = "main";
-strcpy(current_co.name, main_name);
-current_co->main = true;
-current_co->status = CO_RUNNING;
+strcpy((current_co).name, main_name);
+current_co.main = true;
+current_co.status = CO_RUNNING;
 
 void co_delete(struct co* co) {
     for(int i = 0; i < pool_member; i ++) {

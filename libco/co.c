b31ffc9b52267a39bb2b6714b80dd75f26cb83ec
tracer-nju <tracer@nju.edu.cn>
2020-04-05 22:23:05 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: d018140fe02d448f86d3895be82cd1c1   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-45-generic       Architecture: x86-64  22:23:05 up 13:42,  2 users,  load average: 0.71, 0.57, 0.50

--- libco/co.c
+++ libco/co.c
@@ -324,7 +324,7 @@ void co_wait(struct co *co) {
 			assert(current == co);
 			assert(current != NULL);
             
-			stack_xhcg(&(current->stack[STACK_SIZE]);
+			stack_xhcg(&current->stack[STACK_SIZE]);
 	    	current->func(current->arg);
 	    	current->status = CO_DEAD;
 	    	current = NULL;

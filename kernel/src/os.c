004e5bc7c13020bcbe13df35f835ed1c42fcaa09
tracer-nju <tracer@nju.edu.cn>
2020-04-06 11:32:01 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 0cc5597238814d9599133fb9b10f3fa6   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-45-generic       Architecture: x86-64  11:32:01 up  2:45,  4 users,  load average: 0.49, 0.35, 0.21

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -41,6 +41,7 @@ static void os_init() {
 static void os_run() {
   os->init();
   _mpe_init(smoke_test);
+  while (1) ;
   /*
   for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
     _putc(*s == '*' ? '0' + _cpu() : *s);

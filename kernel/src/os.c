77fb48e75dd322452ed45376f938dec3571494c1
tracer-nju <tracer@nju.edu.cn>
2020-04-06 10:05:07 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 0cc5597238814d9599133fb9b10f3fa6   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-45-generic       Architecture: x86-64  10:05:07 up  1:18,  1 user,  load average: 0.47, 0.39, 0.37

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -1,14 +1,42 @@
-#include <common.h>
+#include "common.h"
+#include "klib.h"
+
+/*
+enum ops { OP_ALLOC = 1, OP_FREE };
+struct op {
+    enum ops type;
+	union {
+	    size_t alloc_size;
+		void * free_addr;
+	};
+};
+
+void stress_test() {
+    while(1) {
+	    struct op op = random_op();
+      switch(op.type) {
+        case OP_ALLOC:
+          alloc_check(pmm->alloc(op.alloc_size, op.alloc_size));
+          break;
+        case OP_FREE:
+          free(op.free_addr);
+          break;
+      }
+	}
+}
+*/
 
 static void os_init() {
   pmm->init();
 }
 
 static void os_run() {
+  
   for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
     _putc(*s == '*' ? '0' + _cpu() : *s);
   }
   while (1) ;
+  
 }
 
 MODULE_DEF(os) = {

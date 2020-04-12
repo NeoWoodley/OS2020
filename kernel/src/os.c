762ae871e6884e667ac9fc425735e5c8561cccc4
tracer-nju <tracer@nju.edu.cn>
2020-04-12 10:09:34 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: c938bbc57c054b00862ff12bb90f9d2a   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-46-generic       Architecture: x86-64  10:09:34 up  1:28,  4 users,  load average: 0.27, 0.32, 0.34

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -2,6 +2,7 @@
 #include <klib.h>
 
 
+int i = 0;
 /*
 enum ops { OP_ALLOC = 1, OP_FREE };
 struct op {
@@ -28,25 +29,25 @@ void stress_test() {
 */
 
 void smoke_test() {
-    for(int i = 0; i < 4; i ++){
+    for(; i < 4; i ++){
 		if(i%4 == 0) {
 	        uintptr_t ptr = (uintptr_t)pmm->alloc(sizeof(char));
-		    printf("ptr->char: %p\n", ptr);
+		    printf("ptr->char: %p, I:%d from cpu #%d\n", ptr, i, _cpu());
 		    pmm->free((void*)ptr);	
 		}
 		else if(i%4 == 1) {
 	        uintptr_t ptr = (uintptr_t)pmm->alloc(sizeof(uintptr_t));
-		    printf("ptr->uintptr_t: %p\n", ptr);
+		    printf("ptr->uintptr_t: %p, I:%d from cpu #%d\n", ptr, i, _cpu());
 		    pmm->free((void*)ptr);	
 		}
 		else if(i%4 == 2){
 	        uintptr_t ptr = (uintptr_t)pmm->alloc(sizeof(double));
-		    printf("ptr->double: %p\n", ptr);
+		    printf("ptr->double: %p, I:%d from cpu #%d\n", ptr, i, _cpu());
 		    pmm->free((void*)ptr);	
 		}
 		else {
 	        uintptr_t ptr = (uintptr_t)pmm->alloc(4096*sizeof(char));
-		    printf("ptr->4096: %p\n", ptr);
+		    printf("ptr->4096: %p, I:%d from cpu #%d\n", ptr, i, _cpu());
 		    pmm->free((void*)ptr);	
 		}
 	}
@@ -55,6 +56,7 @@ void smoke_test() {
 
 static void os_init() {
   pmm->init();
+  i = 0;
 }
 
 static void os_run() {

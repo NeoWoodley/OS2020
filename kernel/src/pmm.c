33237d3f7e6fd020224358a40a3713af9e408c30
tracer-nju <tracer@nju.edu.cn>
2020-04-12 23:32:58 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 91bbe28b8ee04e248b6f2429954c6930   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-46-generic       Architecture: x86-64  23:32:58 up 27 min,  3 users,  load average: 9.55, 9.23, 5.24

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -412,6 +412,9 @@ static void kfree(void *ptr) {
 #endif
 
 	       while(*tmp == VALID && (uintptr_t)tmp >= page+sizeof(page_t)) {
+#ifdef CUR
+        printf("H\n");
+#endif
 			   tmp --;
 		   }
 	       //printf("\n");

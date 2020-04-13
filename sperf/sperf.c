323a52cedaecabe981b63e9495f133b0a95a18ef
tracer-nju <tracer@nju.edu.cn>
2020-04-13 22:25:53 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 173d10d0975f4b86985edb3e48d29582   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-46-generic       Architecture: x86-64  22:25:53 up 50 min,  3 users,  load average: 0.18, 0.10, 0.07

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -13,7 +13,7 @@
 int main(int argc, char *argv[]) {
   
   printf("-----------------------\n");
-  for(int i = 0; i < agrc; i ++) {
+  for(int i = 0; i < argc; i ++) {
       printf("%s\n",argv[i]);
   }
   printf("-----------------------\n");

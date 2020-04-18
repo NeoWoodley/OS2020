61efa58d4a1318b3da190fd115bd8f2290498abb
tracer-nju <tracer@nju.edu.cn>
2020-04-18 21:23:23 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 32f5f051a65a4e6ab1968087fae5e1d1   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-46-generic       Architecture: x86-64  21:23:23 up  1:20,  3 users,  load average: 0.26, 0.50, 0.49

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -18,18 +18,18 @@ int main(int argc, char *argv[]) {
   for(int i = 0; i < argc-1; i ++) {
       exec_argv[i] = argv[i+1];
   }
-  /*
   char *exec_envp[] = { "PATH=/bin", NULL, };
   execve("/usr/bin/strace", exec_argv, exec_envp);
   perror(argv[0]);
   exit(EXIT_FAILURE);
-  */
+  /*
   printf("----------argc:%d------------\n",argc);
   for(int i = 0; i < argc - 1; i ++) {
       printf("i:%d\t%s\n",i, exec_argv[i]);
   }
   printf("-----------------------\n");
 	return 0;
+*/
 /*
   char *exec_argv[] = { "strace", "ls", NULL, };
   char *exec_envp[] = { "PATH=/bin", NULL, };

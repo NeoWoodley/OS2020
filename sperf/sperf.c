7dc9ed4f1f4c91b7833ff86694162643c3fd1565
tracer-nju <tracer@nju.edu.cn>
2020-04-20 20:51:07 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 107f26b3cae4417ebff524cbf8dd72b0   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-46-generic       Architecture: x86-64  20:51:07 up 50 min,  3 users,  load average: 0.28, 0.52, 0.50

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -57,7 +57,6 @@ int main(int argc, char *argv[]) {
   else {
 	  close(fildes[1]);
 	  size_t read_length = read(fildes[0], read_buf, sizeof(read_buf));
-	  printf("Read %ld chars\n", read_length);
 	  printf("%s\n", read_buf);
 	  //父进程，读取strace输出并统计
   

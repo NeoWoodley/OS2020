862eea309d3303248eeea50542536817d5f18e47
tracer-nju <tracer@nju.edu.cn>
2020-04-20 22:00:46 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 107f26b3cae4417ebff524cbf8dd72b0   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-46-generic       Architecture: x86-64  22:00:46 up  2:00,  3 users,  load average: 1.42, 1.48, 1.43

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -49,10 +49,8 @@ int main(int argc, char *argv[]) {
   if(pid == 0) {
 	  close(fildes[0]);
 	  //close(2);
-	  dup2(fildes[1], 2);
+	  //dup2(fildes[1], 2);
       //子进程，执行strace命令
-	  char* test = "execve(\"/bin/ls\", [\"ls\"], 0x7ffe725f1fd8 /* 1 var */)";
-	  write(fildes[1], test, strlen(test));
 	  execve("/usr/bin/strace", exec_argv, exec_envp);
 	  //不应该执行此处代码，否则execve失败，出错处理
   }

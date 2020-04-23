7c9d4782bf9e53e6151564a330cb8a99878a5bf4
tracer-nju <tracer@nju.edu.cn>
2020-04-23 19:43:49 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 0deb6942aea145e6b53193c77afa8412   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-46-generic       Architecture: x86-64  19:43:49 up  9:33,  3 users,  load average: 1.68, 1.56, 1.45

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -146,7 +146,7 @@ int readline() {
     int i = 0;
     while(*tmp != '\n' && i < 256 && bound_test(tmp) == 0) {
         line_buf[i] = *tmp;
-		printf("%c?"\n, *tmp);
+		printf("%c?\n", *tmp);
 	    *tmp = '\0';
 	    i ++;
 	    tmp ++;

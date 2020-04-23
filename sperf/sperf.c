1cd3f268d15458e13496ad678493a01d8b59ede8
tracer-nju <tracer@nju.edu.cn>
2020-04-23 20:29:42 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 935ea35d335b45218de3535b8069d4c1   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-46-generic       Architecture: x86-64  20:29:42 up 16 min,  3 users,  load average: 0.06, 0.14, 0.17

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -362,7 +362,7 @@ int main(int argc, char *argv[]) {
 	  //不应该执行此处代码，否则execve失败，出错处理
   }
   else {
-	  sleep(1);
+	  sleep(5);
 	  close(fildes[1]);
 	  int read_length = 0;
 	  read_length = read(fildes[0], read_buf, 10240);

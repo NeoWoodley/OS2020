911ed35bc07076177734b592ef7a4067d9eac235
tracer-nju <tracer@nju.edu.cn>
2020-04-24 17:03:31 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 3a31fef40edd43dcbfff4cea10ce59dd   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-46-generic       Architecture: x86-64  17:03:31 up  1:14,  3 users,  load average: 0.22, 0.27, 0.32

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -366,8 +366,8 @@ int main(int argc, char *argv[]) {
 	  int trash = open("/dev/null", O_RDWR);
 	  dup2(trash, fileno(stdout));
 	  close(trash);
-	  close(fildes[0]);
-	  //dup2(fildes[1], fileno(stderr));
+	  close(fildes[1]);
+	  dup2(fildes[1], fileno(stderr));
 	  char* pwd = strtok(path, ":");
 	  memset(args, '\0', 64);
 	  strcpy(args, pwd);

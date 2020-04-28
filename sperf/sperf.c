da1132cf55a018e07d40be958fe136da8bcf73b4
tracer-nju <tracer@nju.edu.cn>
2020-04-28 20:37:26 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 7db1cb0321754444bb9b92a2160afe2a   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-46-generic       Architecture: x86-64  20:37:27 up  1:31,  3 users,  load average: 0.33, 0.62, 0.64

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -14,19 +14,8 @@
 extern char** environ;
 //#define DETAIL
 //#define CRAZY
-/*
-   int execve(
-       const char *filename,
-	   char *const argv[],
-	   char *const envp[]
-   );
-
-   strace -T 显示系统调用所花时间
-*/
 
 char read_buf[512];
-
-//char test_buf[128];
 
 char left_buf[512];
 
@@ -40,8 +29,6 @@ struct item_t {
     char name[64];
 	double time;
 };
-
-double total_time = 0;
 
 double timeset[128];
 
@@ -150,108 +137,6 @@ bool terminate_detect() {
 	    return true;
 	}
     
-}
-
-/*
-int readline() {
-#ifdef CRAZY
-    printf("[#readline] Begin!\n");
-#endif
-
-    char exit[6] = "exit_";
-
-    linebufsmash();
-
-#ifdef DETAIL
-    printf("MARK 1\n");
-#endif
-
-    char *tmp = read_buf;
-
-    while(*tmp == '\0' && bound_test(tmp) == 0) {
-        tmp ++;
-    }
-
-#ifdef DETAIL
-    printf("MARK 2\n");
-#endif
-
-    int i = 0;
-
-    while(*tmp != '\n' && i < 200 && bound_test(tmp) == 0) {
-        line_buf[i] = *tmp;
-	    *tmp = '\0';
-	    i ++;
-	    tmp ++;
-    }
-
-#ifdef DETAIL
-    printf("MARK 3\n");
-#endif
-
-	if(*tmp == '\n') {
-	    *tmp = '\0';
-	}
-
-	printf("Temp_linebuf:%s\n", line_buf);
-
-#ifdef DETAIL
-    printf("MARK 4\n");
-#endif
-
-	if(strncmp(line_buf, exit, 5) == 0) {
-
-#ifdef CRAZY
-        printf("[*readline] Over!\n");
-#endif
-	    return 1;
-	}
-
-#ifdef DETAIL
-    printf("MARK 5\n");
-#endif
-
-	if(bound_test(tmp) == 1) {
-	    strcpy(left_buf, line_buf);
-		printf("Left:%s\n", left_buf);
-#ifdef CRAZY
-        printf("[*readline] Over!\n");
-#endif
-	    return 3;
-	}
-
-#ifdef DETAIL
-    printf("MARK 6\n");
-#endif
-
-	if(leftbufemptytest() == false) {
-
-#ifdef DETAIL
-    printf("MARK 7\n");
-	printf("left_buf:%s\n", left_buf);
-	printf("line_buf:%s\n", line_buf);
-	printf("left_buf:%lu\n", strlen(left_buf));
-	printf("line_buf:%lu\n", strlen(line_buf));
-#endif
-	    strcat(left_buf, line_buf);
-		linebufsmash();
-		strcpy(line_buf, left_buf);
-		leftbufsmash();
-#ifdef CRAZY
-        printf("[*readline] Over!\n");
-#endif
-		return 0;
-	}
-
-#ifdef CRAZY
-    printf("[*readline] Over!\n");
-#endif
-	return 0;
-}*/
-
-char* readline() {
-    
-	return NULL;
 }
 
 void search_insert(item_t *item) {
@@ -417,10 +302,8 @@ int main(int argc, char *argv[]) {
 #endif
   
   char *path = NULL; 
-  //char path_name[6] = "PATH=";
   char exe_name[8] = "/strace";
   char args[64] = "";
-  //strcpy(PATH, path_name);
   path = getenv("PATH");
   char PATH[strlen(path) + 1];
   strcpy(PATH, path);
@@ -428,10 +311,6 @@ int main(int argc, char *argv[]) {
 #ifdef DEBUG
     printf("PATH:%s\n", PATH);
 #endif
-  //strcat(PATH, path);
-
-  //char *exec_envp[] = { PATH, NULL, };
-
   int fildes[2];
 
   if(pipe(fildes) != 0) {
@@ -443,8 +322,7 @@ int main(int argc, char *argv[]) {
 	  close(fildes[0]);
 	  int trash = open("/dev/null", O_RDWR);
 	  dup2(trash, fileno(stdout));
-	  //close(trash);
-	  //close(fildes[0]);
+	  close(trash);
 	  dup2(fildes[1], fileno(stderr));
 	  char* pwd = strtok(PATH, ":");
 	  memset(args, '\0', 64);
@@ -498,10 +376,8 @@ int main(int argc, char *argv[]) {
 	              if(libitem[i].time == 0.0) {
 	                  break;
 	              }
-			      //Total_time需要刷新
 	              total_time += libitem[i].time;
 	              timeset[i] = libitem[i].time;
-                  //printf("Name: %s, Time elapsed: %f\n", libitem[i].name, libitem[i].time);
               }
 
               qsort(timeset, 128, sizeof(timeset[0]), cmp_descend);
@@ -515,7 +391,6 @@ int main(int argc, char *argv[]) {
 	              printf("%s (%f%%)\n", name, ratio);
               }
               printf("====================\n");
-              //printf("====%s=========%f=======\n", name, ratio);
               for(int i = 0; i < 80; i ++) {
                   printf("%c", 0);
               }
@@ -526,132 +401,7 @@ int main(int argc, char *argv[]) {
 
 		  memset(read_buf, '\0', 512);
 	  }
-	  /*
-	  int len = 0;
-	  //for(int i = 0; i < 1; i ++) {
-	  while(1){
-		  sleep(0.5);
-		  len = read(fildes[0], read_buf, 512);
-		  if(len == 0) {
-		      break;
-		  }
-		  printf("$$$$%d$$$$\n%s\n=====================================================================================\n", len, read_buf);
-		  memset(read_buf, '\0', 512);
-		  fflush(stdout);
-		  //if(read(fildes[0], read_buf, 10240) == 0) {
-		  //  break;		  
-		}
-	      //sleep(1);
-		  //read(fildes[0], read_buf, 10240);
-		  //printf("============================================================================================\n");
-	  }
-	  */
-
-	  //int len = read(fildes[0], read_buf, 10240);
-	  //printf("==================%d===================\n", len);
-	  //printf("%s", read_buf);
-	  //exit(0);
-
-	  // ========================================================
-	  //unsigned read_length = 0;
-	  //read_length = read(fildes[0], read_buf, 200);
-	  //assert(read_length != 0);
-
-	  //printf("Len:%d\n", read_length);
-	  //eofsmash();
-	  //printf("%s\n", test_buf);
-	  //memset(test_buf, '\0', 128);
-
-	  /*
-	  //for(int i = 0; i < 1000; i ++ ) {
-	  while(read_length != 0) {
-		  //sleep(1);
-		  read_length = read(fildes[0], test_buf, 128);
-	      printf("%s", test_buf);
-	      memset(test_buf, '\0', 128);
-		  //printf("____________________%u________________________\n", read_length);
-		  //printf("++++++++++++++++++++++++++++++++++++++++++++++\n");
-	  } 
-	  printf("^*^&*^*&^*&^&&$&$^&*^*(*&(&)&)(&()&*^&*&$^$^\n");
-	  exit(0);
-	  // =========================================================
-      */
-
-/*-----------------------------------------------------------
-	  char* read_length = NULL;
-	  int read_line = 0;
-	  char* name;
-	  double ratio;
-	  int count = 0;
-	  clock_t begin =  clock();
-
-	  while(1) {
-	      read_length = fgets(line_buf, 200, stdin);
-
-		  if(read_length == NULL) {
-		      break;
-		  }
-
-	      eofsmash();
-	      bool output = false;
-
-	      info_extract();
-	  
-          clock_t current = clock();
-
-		  if((current - begin) / 1000 > 1) {
-		      output = true;
-		  }
-          
-		  if(output == true) {
-
-		      total_time = 0;
-              for(int i = 0; i < 128; i ++) {
-	              if(libitem[i].time == 0.0) {
-	                  break;
-	              }
-			      //Total_time需要刷新
-	              total_time += libitem[i].time;
-	              timeset[i] = libitem[i].time;
-                  //printf("Name: %s, Time elapsed: %f\n", libitem[i].name, libitem[i].time);
-              }
-
-              qsort(timeset, 128, sizeof(timeset[0]), cmp_descend);
-		      count ++;
-
-		      printf("Time #%d\n", count);
-              
-			  for(int i = 0; i < 5; i ++) {
-                  name = index_name(timeset[i]);
-                  ratio = ((timeset[i] / total_time) * 100);
-	              printf("%s (%f%%)\n", name, ratio);
-              }
-              printf("====================\n");
-              //printf("====%s=========%f=======\n", name, ratio);
-              for(int i = 0; i < 80; i ++) {
-                  printf("%c", '\0');
-              }
-		      fflush(stdout);
-
-			  begin = clock();
-		  }
-------------------------------------------------------------------*/
-		  //sleep(1);
-//	  for(int i = 0; i < 8; i ++) {
-//		  printf("Line_buf:%s\n", line_buf);
-			  //memset(read_buf, '\0', 128);
-			  //sleep(1);
-			  //memset(read_buf, '\0', 200);
-			  //read_length = read(fildes[0], read_buf, 200);
-	          //eofsmash();
-	          //printf("%s\n", read_buf);
-		      //continue;
-		  //}
-		  //else {
-		   //   break;
-		  //}
 	  //父进程，读取strace输出并统计
-  //   }
   }
 
 

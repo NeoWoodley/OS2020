b6649c1ed6ef6b8092216edadb199e9c929040fe
tracer-nju <tracer@nju.edu.cn>
2020-04-28 19:41:58 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 7db1cb0321754444bb9b92a2160afe2a   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-46-generic       Architecture: x86-64  19:41:58 up 36 min,  3 users,  load average: 0.16, 0.27, 0.29

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -445,7 +445,7 @@ int main(int argc, char *argv[]) {
 	  while(fgets(read_buf, 511, stdin) != NULL) {
 		  eofsmash();
 		  printf("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
-	      printf("%ld  %c", strlen(read_buf), read_buf[strlen(read_buf)-1]);
+	      printf("%ld  %c", strlen(read_buf), read_buf[strlen(read_buf)-2]);
 		  printf("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
 		  info_extract();
 		  for(int i = 0; i < end; i ++) {

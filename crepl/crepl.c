65fbc552969c1362905138a3cdaddc573da7247d
tracer-nju <tracer@nju.edu.cn>
2020-05-18 14:25:03 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: b0bccc7162744fcb941e92f39b413e52   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-51-generic       Architecture: x86-64  14:25:03 up 41 min,  2 users,  load average: 0.22, 0.24, 0.31

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -20,15 +20,22 @@ int main(int argc, char *argv[]) {
 		int tmp_file = mkstemps(template, 2);
 		write(tmp_file, line, strlen(line));
 		char exec_file[] = "gcc";
-		/*
 		int pid = fork();
 		if(pid == 0) {
-		    execlp(exec_file, "gcc", "-fPIC", "-shared", );
+			char libname[32] = "/tmp/lib";
+			char tmp[16];
+			char suffix[4] = ".so";
+		    strncat(tmp, &template[5], 11);
+			strcat(tmp, suffix);
+			strcat(libname, tmp);
+	        printf(".c file: %s\n", template);
+	        printf(".so file: %s\n", libname);
+
+		    //execlp(exec_file, "gcc","-o", "-fPIC", "-shared", template,  );
 		}
-        */
 
 
-	    printf("%s\n", template);
+	    //printf("%s\n", template);
 	}
 	else {
 	    printf("Expr!\n");

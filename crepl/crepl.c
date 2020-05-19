827dd28ff69d884855105359e12cb3300d8eceb2
tracer-nju <tracer@nju.edu.cn>
2020-05-19 09:05:22 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: fe8bd28da77e4bfbaadb6109d18c25ea   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-51-generic       Architecture: x86-64  09:05:22 up 36 min,  1 user,  load average: 0.98, 1.12, 0.95

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -21,7 +21,7 @@ int fac(int base, int time) {
 
 int main(int argc, char *argv[]) {
   printf("argc:%d\n", argc);
-  printf("argv:%s\n", argv);
+  printf("argv:%s\n", *argv);
   static char line[4096];
   static char func[4] = "int";
   static int count = 0;

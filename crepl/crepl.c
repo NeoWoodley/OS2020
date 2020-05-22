4b89418dcf6e994573625f1656f905dd5113025a
tracer-nju <tracer@nju.edu.cn>
2020-05-22 14:24:41 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: bc2c13fc3f45497498a364589f3272dc   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-51-generic       Architecture: x86-64  14:24:41 up  3:44,  2 users,  load average: 0.66, 0.65, 0.71

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -227,7 +227,7 @@ int main(int argc, char *argv[]) {
 		    }
 		    else {
 			    usleep(T);
-		        void* handle = dlopen(libname, RTLD_NOW|RTLD_GLOBAL);
+		        void* handle = dlopen(libname, RTLD_NOW);
 			    assert(handle != NULL);
 
 			    int (*callfunc)();

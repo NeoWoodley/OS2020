886d4d6f3795dea9aa01f330340a14e93ff4dacf
tracer-nju <tracer@nju.edu.cn>
2020-05-28 10:57:43 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: 805dadc4b5c641c0832e8d82ef20cda9   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-53-generic       Architecture: x86-64  10:57:44 up 43 min,  1 user,  load average: 0.35, 0.42, 0.45

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -7,8 +7,17 @@ struct fat_header {
 	uint8_t BS_OEMName[8];
 	uint32_t BPB_BytsPerSec: 16;
 	uint32_t BPB_SecPerClus: 8;
+	uint16_t BPB_RsvdSecCnt;
+	uint8_t BPB_NumFATS;
+	uint16_t BPB_RootEntCnt;
+	uint16_t BPB_TotSec16;
+	uint8_t BPB_Media;
+    uint16_t BPB_FATSz16;
+	uint16_t BPB_SecPerTrk;
+	uint16_t BPB_NumHeads;
+	uint32_t BPB_Hiddsec;
+	uint32_t BPB_TotSec32;
 
-	uint8_t padding[420];
 	uint16_t signature;
 } __attribute__((packed));
 

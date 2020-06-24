a4a3b3e016c43fdad4eb2c1878fe08c65370ccf0
tracer-nju <tracer@nju.edu.cn>
2020-06-24 17:46:22 +0800

    Static hostname: nector-ThinkPad-L460          Icon name: computer-laptop            Chassis: laptop         Machine ID: 683028f5f9d8457e947a981b23a95fa2            Boot ID: c6c3a32f90714ee38b31393a9b529f9a   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 5.3.0-59-generic       Architecture: x86-64  17:46:22 up 14 min,  1 user,  load average: 0.18, 0.57, 0.67

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -2,21 +2,31 @@
 #include <assert.h>
 
 struct kvdb {
+  char* filename;
+  char* key;
+  char value;
   // your definition here
 };
 
 struct kvdb *kvdb_open(const char *filename) {
+  assert(db != NULL);
   return NULL;
 }
 
 int kvdb_close(struct kvdb *db) {
+  assert(db != NULL);
   return -1;
 }
 
 int kvdb_put(struct kvdb *db, const char *key, const char *value) {
+  assert(db != NULL);
+  assert(key != NULL);
+  assert(value != NULL);
   return -1;
 }
 
 char *kvdb_get(struct kvdb *db, const char *key) {
+  assert(db != NULL);
+  assert(key != NULL);
   return NULL;
 }
